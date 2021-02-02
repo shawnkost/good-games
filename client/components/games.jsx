6c3a390f5002899863d38c0e5fff9335bf5eae1a
shawnkost <shawnmkost@gmail.com>
2021-02-02 01:01:25 -0800

    Created game cards, showing game image and critic rating #1

--- client/components/games.jsx
+++ client/components/games.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+const dayjs = require('dayjs');
 
 export default class Games extends React.Component {
   constructor(props) {
@@ -7,24 +8,54 @@ export default class Games extends React.Component {
       games: [],
       platform: 4
     };
+    this.date = dayjs().format('YYYY-MM-DD');
     this.mostPopular = this.mostPopular.bind(this);
+    this.mapGames = this.mapGames.bind(this);
     this.mostPopular();
   }
 
   mostPopular() {
     fetch(
-      `https://api.rawg.io/api/games?platforms=${this.state.platform}&dates=1990-01-01,2021-02-01&ordering=-metacritic&key=c9b670c66eaf4b37bb3b8a3d805fd304`
+      `https://api.rawg.io/api/games?platforms=${this.state.platform}&dates=2000-01-01,${this.date}&ordering=-metacritic&key=c9b670c66eaf4b37bb3b8a3d805fd304`
     )
       .then(response => response.json())
-      .then(games => this.setState({
-        games
-      }));
-    // const date = new Date().toLocaleDateString();
+      .then(games =>
+        this.setState({
+          games
+        })
+      );
+  }
+
+  mapGames() {
+    const listOfGames = this.state.games.results.map((game, index) => {
+      return <CreateGameCard value={game} image={game.background_image} key={index} />;
+    });
+    return listOfGames;
   }
 
   render() {
+    let gameList = '';
+    if (this.state.games.results && this.state.games.results.length > 0) {
+      gameList = this.mapGames();
+    }
     return (
-      <i src=""></i>
+      <div className="container-fluid">
+        {gameList}
+      </div>
     );
   }
+}
+
+function CreateGameCard(props) {
+  return (
+    <div className="game-card">
+      <div className="w-100">
+        <img src={props.image} className="w-100"></img>
+      </div>
+      <div className="d-flex justify-content-between align-items-center text-white details">
+        <div className="ml-3">{props.value.name}</div>
+        <div className="mr-3 metacritic">{props.value.metacritic}</div>
+      </div>
+    </div>
+  );
 }
