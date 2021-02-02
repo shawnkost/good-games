5ef2c099f35ba5a2a542f3cc3a7664e4ce1e592f
shawnkost <shawnmkost@gmail.com>
2021-02-01 18:32:30 -0800

    Adjusted CSS to make navbar look better #1

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -3,11 +3,21 @@ import React from 'react';
 export default class Navbar extends React.Component {
   render() {
     return (
-      <header>
-        <a href="#home" className="text-white">GG</a>
-        <input type="text" id="search" name="search" placeholder="Search"></input>
-        <i className="fas fa-bars"></i>
-      </header>
+      <div className="container-fluid">
+        <div className="row header-row align-items-center">
+            <a href="#home" className="col-4 text-white logo">
+              GG
+            </a>
+            <input
+              type="text"
+              id="search"
+              name="search"
+              placeholder="Search"
+              className="col-4 border-radius"
+            ></input>
+            <i className="fas fa-bars col-4 text-white menu text-align-right"></i>
+        </div>
+      </div>
     );
   }
 }
