3b263cc5af0f6fd266e931fb7215eb9ae460e3a6
shawnkost <shawnmkost@gmail.com>
2021-02-03 11:28:28 -0800

    Updated the navbar component to include an anchor tag #2

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
 
 export default class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+
+  }
+
   render() {
     return (
       <div className="container-fluid mb-4">
         <div className="row py-2 align-items-center header-row">
-            <a href="#home" className="col-4 text-white logo">
-              GG
-            </a>
-            <input
-              type="text"
-              id="search"
-              name="search"
-              placeholder="Search"
-              className="col-4 search-input"
-            ></input>
-            <i className="fas fa-bars col-4 text-white menu text-right"></i>
+          <a href="#home" className="col-4 text-white logo">
+            GG
+          </a>
+          <input
+            type="text"
+            id="search"
+            name="search"
+            placeholder="Search"
+            className="col-4 search-input"
+          ></input>
+          <a className="col-4 text-right" href="#menu">
+            <i
+              className="fas fa-bars text-white menu"
+              onClick={this.handleClick}
+            ></i>
+          </a>
         </div>
       </div>
     );
