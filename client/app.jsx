9ed7ba44510cb1cea4f6df93470bf2284fafaaf0
shawnkost <shawnmkost@gmail.com>
2021-02-03 11:34:58 -0800

    Imported the parse route function, and updated state in app.jsx with the window hash #2

--- client/app.jsx
+++ client/app.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import Home from './pages/home';
+import parseRoute from './lib/parse-route';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
   render() {
     return <Home />;
   }
