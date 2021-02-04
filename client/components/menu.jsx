bad0f21d0def1c582b008d75625b0555e8228f6b
shawnkost <shawnmkost@gmail.com>
2021-02-03 23:33:15 -0800

    Adjusted styling for the menu #3

--- client/components/menu.jsx
+++ client/components/menu.jsx
@@ -24,25 +24,25 @@ export default class Menu extends React.Component {
           ></i>
           <a
             href="#"
-            className="d-block menu-items mb-4"
+            className="d-block menu-items pl-2 pt-2 pb-2 mb-2"
             onClick={this.handleClick}
           >
             Most Popular
           </a>
           <a
             href="#new-releases"
-            className="d-block menu-items mb-4"
+            className="d-block menu-items pl-2 pt-2 pb-2 mb-2"
             onClick={this.handleClick}
           >
             New Releases
           </a>
-          <a href="#upcoming-games" className="d-block menu-items mb-4">
-            Upcoming Games
+          <a href="#upcoming-games" className="d-block menu-items pl-2 pt-2 pb-2 mb-2">
+            Upcoming
           </a>
-          <a href="#new-releases" className="d-block menu-items mb-4">
+          <a href="#new-releases" className="d-block menu-items pl-2 pt-2 pb-2 mb-2">
             Profile
           </a>
-          <a href="#new-releases" className="d-block menu-items">
+          <a href="#new-releases" className="d-block menu-items pl-2 pt-2 pb-2 mb-2">
             Contact Me
           </a>
         </div>
