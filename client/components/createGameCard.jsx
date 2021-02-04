c223470f61165afe69dd4e5fda6c6d30e6f6faf0
shawnkost <shawnmkost@gmail.com>
2021-02-03 17:24:37 -0800

    Updated gamecard to include links to each stores website #3

--- client/components/createGameCard.jsx
+++ client/components/createGameCard.jsx
@@ -4,48 +4,66 @@ import Playstation from '../images/playstation.png';
 import Xbox from '../images/xbox.png';
 import Windows from '../images/windows.png';
 import Nintendo from '../images/nintendo.png';
-
 export default function CreateGameCard(props) {
   return (
     <div className="game-card mb-3">
+      {' '}
       <div className="w-100 image-container">
-        <img src={props.image} className="w-100 h-100 game-image"></img>
-      </div>
+        {' '}
+        <img src={props.image} className="w-100 h-100 game-image"></img>{' '}
+      </div>{' '}
       <div className="text-white details">
+        {' '}
         <div className="all-platforms-container mb-2">
+          {' '}
           {props.value.parent_platforms.map((platform, index) => {
             let platformImg = null;
+            let platformURL = null;
             switch (platform.platform.slug) {
               case 'pc':
                 platformImg = Windows;
+                platformURL =
+                  'https://www.microsoft.com/en-us/store/games/windows';
                 break;
               case 'xbox':
                 platformImg = Xbox;
+                platformURL = 'https://www.xbox.com/en-US/microsoft-store';
                 break;
               case 'playstation':
                 platformImg = Playstation;
+                platformURL = 'https://store.playstation.com/en-us/latest';
                 break;
               case 'nintendo':
                 platformImg = Nintendo;
+                platformURL = 'https://www.nintendo.com/games/';
                 break;
               default:
                 return null;
             }
             return (
               <div className="platform-container" key={index}>
-                <img src={platformImg} className="w-100 mb-2"></img>
+                {' '}
+                <a href={platformURL} target="_blank" rel="noreferrer">
+                  {' '}
+                  <img src={platformImg} className="w-100 mb-2"></img>{' '}
+                </a>{' '}
               </div>
             );
-          })}
-        </div>
+          })}{' '}
+        </div>{' '}
         <div className="details-container">
-          <div className="pl-1 mb-2">{props.value.name}</div>
-          <div className="pl-1 d-inline-block details-font">{'Release date: ' + dayjs(props.value.released).format('MMM-DD-YYYY')}
-          </div>
-          <div className="d-inline-block view-details pr-1 details-font">View Details</div>
-          <div className="metacritic">{props.value.metacritic}</div>
-        </div>
-      </div>
+          {' '}
+          <div className="pl-1 mb-2">{props.value.name}</div>{' '}
+          <div className="pl-1 d-inline-block details-font">
+            {'Release date: ' +
+              dayjs(props.value.released).format('MMM-DD-YYYY')}{' '}
+          </div>{' '}
+          <div className="d-inline-block view-details pr-1 details-font">
+            View Details
+          </div>{' '}
+          <div className="metacritic">{props.value.metacritic}</div>{' '}
+        </div>{' '}
+      </div>{' '}
     </div>
   );
 }
