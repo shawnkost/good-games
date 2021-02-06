a0b3e90500d546057ab0fbd243b7599b08b001ba
shawnkost <shawnmkost@gmail.com>
2021-02-05 16:48:52 -0800

    Added an end point to save reviews from the user in the database #5

--- database/data.sql
+++ database/data.sql
@@ -1,2 +1,5 @@
 insert into "users" ("username", "email", "password")
 values ('shaaka', 'email123@gmail.com', 'password1');
+
+insert into "reviews" ("gameId", "details", "userId")
+values ('28', 'The characters are extremely diverse and the environment is beautiful', '1');
--- server/index.js
+++ server/index.js
@@ -1,10 +1,40 @@
 require('dotenv/config');
+const pg = require('pg');
 const express = require('express');
+const ClientError = require('./client-error');
+const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL
+});
 
 const app = express();
 
+app.use(express.json());
+
 app.use(staticMiddleware);
+
+app.post('/api/games/reviews', (req, res, next) => {
+  const { gameId, details, userId } = req.body;
+  if (!gameId || !details || !userId) {
+    throw new ClientError(400, 'Invalid gameID/Review/UserId');
+  }
+  const sql = `
+    insert into "reviews" ("gameId", "details", "userId")
+    values ($1, $2, $3)
+    returning *
+    `;
+  const params = [gameId, details, userId];
+  db.query(sql, params)
+    .then(result => {
+      const [review] = result.rows;
+      res.status(201).json(review);
+    })
+    .catch(err => next(err));
+});
+
+app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
