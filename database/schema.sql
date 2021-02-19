6d1ceb34a5c72d4a5535aef0d3bbb9fe1882c313
shawnkost <shawnmkost@gmail.com>
2021-02-19 13:24:57 -0800

    Changed schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,3 +1,11 @@
+set client_min_messages to warning;
+
+-- DANGER: this is NOT how to do it in the real world.
+-- `drop schema` INSTANTLY ERASES EVERYTHING.
+drop schema "public" cascade;
+
+create schema "public";
+
 CREATE TABLE "users" (
 	"userId" serial NOT NULL,
 	"username" TEXT NOT NULL,
